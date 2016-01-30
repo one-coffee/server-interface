5e6818f3b515159533931e1b83b994b78c7af069
Martin Morávek (moravek.martin) <moravek.martin@gmail.com>
2016-01-30 22:41:30 +0100

    pagination for logs

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -4,8 +4,8 @@ const { service } = Ember.inject;
 export default Ember.Controller.extend({
   session: service(),
 
-  email: 'test@email.com',
-  password: 'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff',
+  email: 'hello@keo.tz',
+  password: 'super-password',
   actions: {
     authenticate() {
       let { email, password } = this.getProperties('email', 'password');
--- app/routes/node/logs.js
+++ app/routes/node/logs.js
@@ -1,9 +1,16 @@
 import Ember from 'ember';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
-export default Ember.Route.extend({
-  model() {
-    const node = this.modelFor('node');
-    return this.get('store').query('log', {node: node.get('id')});
+export default Ember.Route.extend(RouteMixin, {
+  model(params) {
+    params.node = this.modelFor('node').get('id');
+    params.order = '-id';
+    params.paramMapping = {
+      page: "page",
+      perPage: "perPage",
+      total_pages: "totalPageCount"
+    };
+    return this.findPaged('log', params);
   },
   setupController(controller) {
     this._super(...arguments);
